87c167fb676a8f0f53d0d7d985ac1c2e7e493d32
Jaya Parmar <jaya.parmar@ivoclar.com>
2023-08-25 10:43:39 +0200

    app.py

--- security/app.py
+++ security/app.py
@@ -9,5 +9,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8444, debug=True, ssl_context=('server-signed-cert1.pem', 'server-key.pem'))
+    app.run(host="0.0.0.0",port=8444, debug=True, ssl_context=('keys/server-signed-cert1.pem', 'keys/server-key.pem'))
     
